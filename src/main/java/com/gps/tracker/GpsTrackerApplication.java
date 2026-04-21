4587a7acb445f5405f97cc194014e01c8470d704
ntd1911 <duongc1lhp2015@gmail.com>
2026-04-21 14:50:09 +0700

    version 1 app

---
+++ src/main/java/com/gps/tracker/GpsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gps.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gps/tracker/GpsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gps.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
